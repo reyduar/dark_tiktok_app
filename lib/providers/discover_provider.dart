1d26183a5d588fb6151365685040975679431dd4
ariel.duarte <arielpy@gmail.com>
2023-11-02 21:44:09 -0300

    Added mechanism to display and pause videos

--- lib/providers/discover_provider.dart
+++ lib/providers/discover_provider.dart
@@ -1,13 +1,23 @@
 import 'package:dark_tiktok_app/domain/entities/video_post.dart';
+import 'package:dark_tiktok_app/infrastructure/models/local_video_model.dart';
+import 'package:dark_tiktok_app/shared/data/local_video_posts.dart';
 import 'package:flutter/material.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videosList = [];
 
   Future<void> loadNextPage() async {
-    // cargar videos
+    // await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videosList.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
